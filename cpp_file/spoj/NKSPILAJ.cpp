651c3473c182b8cbf392c6018b7f5e1ac5899b20
user557742 <longduonggia304@gmail.com>
2025-04-13 15:10:23 +0700

    Create NKSPILAJ.cpp

---
+++ cpp_file/spoj/NKSPILAJ.cpp
@@ -0,0 +1,20 @@
+#include <bits\stdc++.h>
+using namespace std;
+int n;
+const int N = 5001;
+int a[N],b[N];
+struct line{
+    double x,double y;
+};
+line mline(int u,int v,int g,int h){
+    
+}
+bool cat(line u,line v){
+    if(u.x==v.x) return 0;
+    double nx=1.0*(v.y-u.y)/(u.x-v.x);
+    double ny=1.0*(v.y*u.x-u.y*v.x)/(u.x-v.x);
+    
+}
+int main(){
+    return 0;
+}
